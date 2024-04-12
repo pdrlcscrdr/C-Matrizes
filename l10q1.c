c44e72e7dd37344e4aa7862f8c07a69e1f363b6b
pdrlcscrdr <166539206+pdrlcscrdr@users.noreply.github.com>
2024-04-12 10:33:25 -0300

    Exemplo de código que utiliza uma matriz.
    Armazena 9 valores em uma matriz e exibe a matriz em ordem inversa.

---
+++ l10q1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int l, c, x = 1, A[3][3];
+	for(l = 0; l < 3; l++){
+		for(c = 0; c < 3; c++){
+			printf("Digite o %i° valor inteiro: \n", x++);
+			scanf("%i", &A[l][c]);
+		}
+	}
+	printf("\nMatriz invertida\n");
+	for(l = 2; l >= 0; l--){
+		for(c = 2; c >= 0; c--){
+			printf("%i  ", A[l][c]);
+		}
+		printf("\n");
+	}
+	return(0);
+}
