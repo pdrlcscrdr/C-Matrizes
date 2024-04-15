bceeb3dffc9d60c595646ac7d39c6aec18f44fa6
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:48:09 -0300

    Inversão de valores.c

--- Inversão de valores.c
+++ Inversão de valores.c
@@ -5,17 +5,20 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float A[4][2], B[4][2];
 	int l, c, x = 1;
+	//Armazenamento de valores em uma matriz 
 	for(l = 0; l < 4; l++){
 		for(c=0; c < 2; c++){
-			printf("Digite o %i� valor para armazenar na matriz A: \n", x++);
+			printf("Digite o %i° valor para armazenar na matriz A: \n", x++);
 			scanf("%f", &A[l][c]);
 		}
 	}
+	//Preenchendo outra matriz com o inverso da primeira matriz
 	for(l = 0; l < 4; l++){
 		for(c = 0; c < 2; c++){
 			B[3 - l][1 - c] = A[l][c];
 		}
 	}
+	//Exibiçaõ da matriz A
 	printf("\n\n");
 	printf("\nMatriz A\n");
 	for(l = 0; l < 4; l++){
@@ -24,6 +27,7 @@ int main(){
 		}
 		printf("\n");
 	}
+	//Exibição da matriz B
 	printf("\nMatriz B\n");
 	for(l = 0; l < 4; l++){
 		for(c=0; c < 2; c++){
