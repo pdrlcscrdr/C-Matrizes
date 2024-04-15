bda37481ff733069f7f58f1f16cc7f136cfa5182
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:45:03 -0300

    Médias de 5 alunos.c

--- Médias de 5 alunos.c
+++ Médias de 5 alunos.c
@@ -6,25 +6,28 @@ int main(){
 	char nome[5][50];
 	float notas[5][3], media[5], m = 0;
 	int l, c, x = 1;
+	//Armazenamento de nomes 
 	for(l = 0; l < 5; l++){
-	printf("Informe o nome do %i° aluno:  \n", x++);
+	printf("Informe o nome do %iÂ° aluno:  \n", x++);
 	gets(nome[l]);
 	}
 	printf("\n-----------------------------------\n");
+	//Armazenamento de notas
 	for(l = 0; l < 5; l++){
-		printf("Digite as três notas de %s\n", nome[l]);
+		printf("Digite as trÃªs notas de %s\n", nome[l]);
 		m = 0;
 		for(c = 0; c < 3; c++){
-			printf("%iª: ", c + 1);
+			printf("%iÂª: ", c + 1);
 			scanf("%f", &notas[l][c]);
 			m += notas[l][c];
 		}
 		media[l] = m / 3;
 		printf("\n");
 	}
-	printf("\n\n----------------------------------\n\n*** Lista de alunos e médias***\n\n");
+	//ExibiÃ§Ã£o de mÃ©dias
+	printf("\n\n----------------------------------\n\n*** Lista de alunos e mÃ©dias***\n\n");
 	for(l = 0; l < 5; l++){
-		printf("%iª: %s, você obteve média %.1f. \n", l + 1, nome[l], media[l]);
+		printf("%iÂª: %s, vocÃª obteve mÃ©dia %.1f. \n", l + 1, nome[l], media[l]);
 	}
 	return(0);
 }
