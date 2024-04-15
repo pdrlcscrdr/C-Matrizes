1a3873746bbc4b3d3fe87fabc1178a803d77dfc0
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:40:46 -0300

    Inverso da matriz criada.c

--- Inverso da matriz criada.c
+++ Inverso da matriz criada.c
@@ -4,12 +4,14 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int l, c, x = 1, A[3][3];
+	//Usuário cria a matriz
 	for(l = 0; l < 3; l++){
 		for(c = 0; c < 3; c++){
-			printf("Digite o %i� valor inteiro: \n", x++);
+			printf("Digite o %i° valor inteiro: \n", x++);
 			scanf("%i", &A[l][c]);
 		}
 	}
+	//Exibição de uma matriz com os valores invertidos
 	printf("\nMatriz invertida\n");
 	for(l = 2; l >= 0; l--){
 		for(c = 2; c >= 0; c--){
