9d5e7f76a3f9f4ad6183fc1c602a760962fad8f2
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:42:56 -0300

    Criação e exibição de duas matrizes.c

--- Criação e exibição de duas matrizes.c
+++ Criação e exibição de duas matrizes.c
@@ -4,21 +4,24 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int A[2][3], B[2][3], l, c, x = 1;
+	//Criação da primeira matriz
 	for(l = 0; l < 2; l++){
 		for(c=0; c < 3; c++){
-			printf("Digite o %i� valor para armazenar na matriz A: \n", x++);
+			printf("Digite o %i° valor para armazenar na matriz A: \n", x++);
 			scanf("%i", &A[l][c]);
 		}
 	}
 	printf("\n\n");
+	//Criação da segunda matriz
 	x = 1;
 	for(l = 0; l < 2; l++){
 		for(c=0; c < 3; c++){
-			printf("Digite o %i� valor para armazenar na matriz B: \n", x++);
+			printf("Digite o %i° valor para armazenar na matriz B: \n", x++);
 			scanf("%i", &B[l][c]);
 		}
 	}
 	printf("\n\n");
+	//Exibição da primeira matriz
 	printf("\nMatriz A\n");
 	for(l = 0; l < 2; l++){
 		for(c=0; c < 3; c++){
@@ -26,6 +29,7 @@ int main(){
 		}
 		printf("\n");
 	}
+	//Exibição da segunda matriz
 	printf("\nMatriz B\n");
 	for(l = 0; l < 2; l++){
 		for(c=0; c < 3; c++){
